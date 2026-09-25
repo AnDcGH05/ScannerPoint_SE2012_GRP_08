6b1c23a5637bbeb5bbe8941190a97223faabcb81
Your Name <your.email@example.com>
2026-09-25 16:00:06 +0530

    Initial Spring Boot setup

---
+++ src/main/java/ScannerPoint/example/ScannerPoint/ScannerPointApplication.java
@@ -0,0 +1,13 @@
+package ScannerPoint.example.ScannerPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScannerPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScannerPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ScannerPoint/example/ScannerPoint/ScannerPointApplicationTests.java
@@ -0,0 +1,13 @@
+package ScannerPoint.example.ScannerPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScannerPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
